9cf466defde0d7388db69ebec5ced57827df4c7d
Manvith Kumar shetty <121666002+manvithshetty507@users.noreply.github.com>
2024-01-20 13:12:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const year = prompt('Enter a year')
+
+function isLeapYear(year) {
+    if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+        return 366; 
+    }
+    return 365; 
+}
+
+alert(isLeapYear(year))
